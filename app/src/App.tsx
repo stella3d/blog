a2ed80f84ca37bffd47b6c79ab80931a65cde004
Stella <stella@ondora.xyz>
2025-03-29 12:42:09 -0500

    fix some style issues, move test post

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import PostRenderer from './PostRenderer'
 
 function App() {
   const [postContent, setPostContent] = useState('');
 
+  // Fetch default markdown content on mount
+  useEffect(() => {
+    fetch('/test_posts/helloworld.md')
+      .then(res => res.text())
+      .then(text => setPostContent(text))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <>
-      <h1>stellz' blog</h1>
-
-      <textarea 
-        id="postinput" 
-        value={postContent} 
-        onChange={(e) => setPostContent(e.target.value)}
-      />
+      <h1 id="headtext">stellz' blog</h1>
 
       <PostRenderer content={postContent}></PostRenderer>
     </>
