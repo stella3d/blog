6d8e95cc9e67014a5b23ad00cb0df27be4e65e1b
Stella <stella@ondora.xyz>
2025-03-29 16:10:46 -0500

    add postrecord type

--- app/src/types.ts
+++ app/src/types.ts
@@ -39,3 +39,30 @@ export type PostIndexEntry = {
     uri: string; // URI for accessing the post
   };
 }
+
+export type PostRecord = {
+    $type: 'beauty.piss.blog.entry';
+    /** Maximum string length: 420 */
+    title: string;
+    /** Maximum string length: 200000 */
+    content: string;
+    //images?: BeautyPissEmbedImages.Record;
+    tags?: string[]; 
+    /** ISO string */
+    createdAt: string;
+}
+
+export function isPostRecord(obj: any): obj is PostRecord {
+    return (
+        obj &&
+        typeof obj === 'object' &&
+        '$type' in obj &&
+        obj.$type === 'beauty.piss.blog.entry' &&
+        'title' in obj &&
+        typeof obj.title === 'string' &&
+        'content' in obj &&
+        typeof obj.content === 'string' &&
+        'createdAt' in obj &&
+        typeof obj.createdAt === 'string'
+    );
+}
