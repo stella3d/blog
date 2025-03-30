835b3d82f38e19dd7ee020de8b107c3f2f2e4020
Stella <stella@ondora.xyz>
2025-03-29 19:16:04 -0500

    change vite build to be one dir

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    // Set assetsDir to empty so that no extra directory is created
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        // This tells Rollup not to nest asset files inside an "assets" directory
+        assetFileNames: '[name].[ext]',
+      }
+    }
+  }
 })
