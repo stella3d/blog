cf5c7f4beb87d34fdbf6e650141dc45a940246d5
Stella <stella@ondora.xyz>
2025-03-31 10:36:37 -0500

    make 404 page a copy of index

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'  
+import fs from 'fs'
+import path from 'path'
+
+// Custom plugin to copy index.html to 404.html after build
+function copyIndexTo404Plugin(): PluginOption {
+  return {
+    name: 'copy-index-to-404',
+    writeBundle(options: { dir: any }) {
+      const outDir = options.dir;
+      const indexPath = path.resolve(outDir, 'index.html');
+      const notFoundPath = path.resolve(outDir, '404.html');
+      if (fs.existsSync(indexPath)) {
+        fs.copyFileSync(indexPath, notFoundPath);
+        //console.log('Copied index.html to 404.html');
+      }
+    }
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        { src: './404.html', dest: '' } 
-      ]
-    })
+    copyIndexTo404Plugin()
   ],
   build: {
     outDir: 'dist',
