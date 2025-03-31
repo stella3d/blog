322ddf792331882a5c466e8b97840fdaa16add0c
Stella <stella@ondora.xyz>
2025-03-31 09:38:32 -0500

    add 404 page to build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'  
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: './404.html', dest: '' } 
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
     // Set assetsDir to empty so that no extra directory is created
