81458c335885971537de4177524121e8987e646f
Stella <stella@ondora.xyz>
2025-03-31 11:17:51 -0500

    accept slug as query param

--- app/src/slugs.ts
+++ app/src/slugs.ts
@@ -9,6 +9,14 @@ export function slugify(title: string): string {
 
 
 export function getSlugFromUrl(): string {
+    // if "p" or "post" is in the query string, use that first
+    const queryParams = new URLSearchParams(window.location.search);
+    if (queryParams.has('p')) {
+        return queryParams.get('p') || ''; // Return empty string if 'p' is empty
+    } else if (queryParams.has('post')) {
+        return queryParams.get('post') || '';
+    }
+
     // Extract the slug from the current URL
     const path = window.location.pathname;
     const segments = path.split('/');
